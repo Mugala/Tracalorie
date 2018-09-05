e2dd1d70d0d50b33a8e224cb8e000342dd99c257
mugala <mugalalevi21@gmail.com>
2018-09-05 10:15:41 +0300

    IIFE Functions This commit will create the IIFE functions for the Item App and UI controllers

---
+++ app.js
@@ -0,0 +1,15 @@
+// Storage Controller
+
+//Item Controller
+const ItemCtrl = (function(){
+  
+})();
+
+//IU Controller
+const UICtrl = (function(){
+  
+})();
+//App Controller
+const App = (function(){
+  
+})();
