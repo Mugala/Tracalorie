c6ceabedf956aa17dd5dd22175ac887a1581aba9
mugala <mugalalevi21@gmail.com>
2018-09-10 16:44:46 +0300

    Update items in local storage This commit will enable user to update items in the local storage and save

--- app.js
+++ app.js
@@ -1,58 +1,51 @@
-/*=========================================================================================================================================================
-Storage Controller
-*/ 
-
+// Storage Controller
 const StorageCtrl = (function(){
-
-  //Public methods
-  return{
+  // Public methods
+  return {
     storeItem: function(item){
       let items;
-      //Check if any items in ls
-
-      if(localStorage.getItem('items')=== null){
+      // Check if any items in ls
+      if(localStorage.getItem('items') === null){
         items = [];
-        //push new item
+        // Push new item
         items.push(item);
-
-        //Set ls
-        localStorag  e.setItem('items', JSON.stringify(items));
-      }else{
-        //Get what is on ls
+        // Set ls
+        localStorage.setItem('items', JSON.stringify(items));
+      } else {
+        // Get what is already in ls
         items = JSON.parse(localStorage.getItem('items'));
 
-        //Push new item 
-        item.push(item);
+        // Push new item
+        items.push(item);
 
-        //Reset ls
+        // Re set ls
         localStorage.setItem('items', JSON.stringify(items));
       }
-
     },
     getItemsFromStorage: function(){
       let items;
-
-      if(localStorage.getItem('items')===null){
+      if(localStorage.getItem('items') === null){
         items = [];
-
-      }else{
+      } else {
         items = JSON.parse(localStorage.getItem('items'));
       }
       return items;
-    }
+    },
+    updateItemStorage: function(updatedItem){
+      let items = JSON.parse(localStorage.getItem('items'));
 
+      items.forEach(function(item,index){
+        if(updatedItem.id == item.id){
+          items.splice(index, 1, updatedItem);
+        }
+      });
+      localStorage.setItem('items', JSON.stringify(items));
+    }
   }
 })();
 
 
-//=======================================================================================================================================================
-
-
-
-/*=========================================================================================================================================================
-Item Controller
-*/ //=======================================================================================================================================================
-
+// Item Controller
 const ItemCtrl = (function(){
   // Item Constructor
   const Item = function(id, name, calories){
@@ -63,11 +56,12 @@ const ItemCtrl = (function(){
 
   // Data Structure / State
   const data = {
-    items: [
-      // {id: 0, name: 'Steak Dinner', calories: 1200},
-      // {id: 1, name: 'Cookie', calories: 400},
-      // {id: 2, name: 'Eggs', calories: 300}
-    ],
+    // items: [
+    //   // {id: 0, name: 'Steak Dinner', calories: 1200},
+    //   // {id: 1, name: 'Cookie', calories: 400},
+    //   // {id: 2, name: 'Eggs', calories: 300}
+    // ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0
   }
@@ -163,12 +157,9 @@ const ItemCtrl = (function(){
   }
 })();
 
-//=========================================================================================================================================================
 
-/*=========================================================================================================================================================
-IU Controller
-*/ //======================================================================================================================================================
 
+// UI Controller
 const UICtrl = (function(){
   const UISelectors = {
     itemList: '#item-list',
@@ -289,13 +280,9 @@ const UICtrl = (function(){
   }
 })();
 
-//=======================================================================================================================================================
 
 
-/*========================================================================================================================================================
-App Controller
-*/ //=====================================================================================================================================================
-
+// App Controller
 const App = (function(ItemCtrl, StorageCtrl, UICtrl){
   // Load event listeners
   const loadEventListeners = function(){
@@ -398,6 +385,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
      // Add total calories to UI
      UICtrl.showTotalCalories(totalCalories);
 
+     //Update local storage
+     StorageCtrl.updateItemStorage(updatedItem);
+
      UICtrl.clearEditState();
 
     e.preventDefault();
@@ -470,8 +460,7 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
     }
   }
   
-})(ItemCtrl,StorageCtrl, UICtrl);
+})(ItemCtrl, StorageCtrl, UICtrl);
 
 // Initialize App
 App.init();
-//========================================================================================================================================================
